234e85f629d4bfe01c8abbce4d74adfd0efdadcb
josejuan <dev@computer-mind.com>
2019-05-21 18:07:10 +0200

    reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,32 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
 module Main where
 
 import Data.Monoid((<>))
 import Control.Monad (forever)
-
-import Network.Wai.Handler.Warp       (run)
+import Network.Wai.Handler.Warp (run)
 import Network.Wai.Application.Static (staticApp, defaultWebAppSettings)
-import Network.WebSockets             (acceptRequest, receiveData, sendTextData, defaultConnectionOptions, PendingConnection)
+import Network.WebSockets (acceptRequest, receiveData, sendTextData, defaultConnectionOptions, PendingConnection)
 import Network.Wai.Handler.WebSockets (websocketsOr)
-
 import qualified Graphics.X11.Xlib as X11
-import qualified XTest             as X11
+import qualified XTest as X11
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import Data.Text.Read (signed, decimal)
 
-import           Data.Text        (Text)
-import qualified Data.Text      as Text
-import qualified Data.Text.IO   as Text
-import           Data.Text.Read   (signed, decimal)
+mouseDelta display window dx dy = X11.selectInput display window X11.keyReleaseMask
+                               >> X11.warpPointer display 0 0 0 0 0 0 dx dy
+                               >> X11.flush display
 
-mouseDelta display window dx dy = do
-  X11.selectInput display window X11.keyReleaseMask
-  X11.warpPointer display 0 0 0 0 0 0 dx dy
-  X11.flush display
-
-mouseButton display button mode = do
-  X11.xFakeButtonEvent display button (mode == "down") 0
-  X11.flush display
+mouseButton display button mode = X11.xFakeButtonEvent display button (mode == "down") 0
+                               >> X11.flush display
 
 mouseScroll display d = do
   let button = if d < 0 then 4 else 5
-  mapM_ (\_ -> X11.xFakeButtonEvent display button True  0
-            >> X11.xFakeButtonEvent display button False 0) [1..abs d]
+  mapM_ (\_ -> X11.xFakeButtonEvent display button True 0 >> X11.xFakeButtonEvent display button False 0) [1..abs d]
   X11.flush display
 
-keyAction display down code = do
-  X11.xFakeKeyEvent display code down 0
-  X11.flush display
+keyAction display down code = X11.xFakeKeyEvent display code down 0 >> X11.flush display
 
 int int = case signed decimal int of
             Right (n, "") -> Right n
